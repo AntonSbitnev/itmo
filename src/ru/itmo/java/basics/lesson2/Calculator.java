76a80d058bf4522fb9b89aa582424cd576869d77
Антон Сбитнев <balamut86@list.ru>
2023-10-27 21:32:48 +0300

    add lesson2

---
+++ src/ru/itmo/java/basics/lesson2/Calculator.java
@@ -0,0 +1,54 @@
+package ru.itmo.java.basics.lesson2;
+
+public class Calculator {
+
+    public double sum(double a, double b) {
+        return a + b;
+    }
+
+    public double sum(long a, long b) {
+        return a + b;
+    }
+
+    public double sum(int a, int b) {
+        return a + b;
+    }
+
+    public double divide(double a, double b) {
+        return a / b;
+    }
+
+    public double divide(long a, long b) {
+        return (double) a / b;
+    }
+
+    public double divide(int a, int b){
+        return (double) a / b;
+    }
+
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+
+    public double multiply(long a, long b) {
+        return a * b;
+    }
+
+    public double multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+
+    public double subtract(long a, long b) {
+        return a - b;
+    }
+
+    public double subtract(int a, int b) {
+        return a - b;
+    }
+
+
+}
