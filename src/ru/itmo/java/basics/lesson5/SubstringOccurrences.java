65d1863a26c68c2f90acc319cd682ab225c717e7
Антон Сбитнев <balamut86@list.ru>
2023-11-06 14:04:30 +0300

    add lesson5

---
+++ src/ru/itmo/java/basics/lesson5/SubstringOccurrences.java
@@ -0,0 +1,32 @@
+package ru.itmo.java.basics.lesson5;
+
+public class SubstringOccurrences {
+    public static int countSubstringOccurrences(String text, String substring) {
+        int count = 0;
+        int index = text.indexOf(substring);
+        while (index != -1) {
+            count++;
+            index = text.indexOf(substring, index + 1);
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        String text = "Вот дом,\n" +
+                "Который построил Джек.\n" +
+                "А это пшеница,\n" +
+                "Которая в темном чулане хранится\n" +
+                "В доме,\n" +
+                "Который построил Джек.\n" +
+                "А это веселая птица-синица,\n" +
+                "Которая часто ворует пшеницу,\n" +
+                "Которая в темном чулане хранится\n" +
+                "В доме,\n" +
+                "Который построил Джек.";
+        String substring = "Джек";
+
+        int occurrences = countSubstringOccurrences(text, substring);
+
+        System.out.println("Количество вхождений подстроки \"" + substring + "\": " + occurrences);
+    }
+}
