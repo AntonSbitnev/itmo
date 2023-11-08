0f3c9cb8667d729215b832a5214b7e827682ac3d
Антон Сбитнев <balamut86@list.ru>
2023-11-08 23:27:48 +0300

    added lesson7

---
+++ src/ru/itmo/java/basics/lesson7/Airplane.java
@@ -0,0 +1,28 @@
+package ru.itmo.java.basics.lesson7;
+
+public class Airplane {
+
+    public class Wing {
+        private double weigth;
+
+        public Wing(double weigth) {
+            this.weigth = weigth;
+        }
+
+        public double showWeigth() {
+            return weigth;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Airplane airplane1 = new Airplane();
+        Wing wing1 = airplane1.new Wing(500.0);
+
+        Airplane airplane2 = new Airplane();
+        Wing wing2 = airplane2.new Wing(600.0);
+
+        System.out.println("Вес первого крыла: " + wing1.showWeigth() + " кг");
+        System.out.println("Вес второго крыла: " + wing2.showWeigth() + " кг");
+    }
+}
